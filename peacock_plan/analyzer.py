2b626e17a399f5566b99adab797d1d65df627e65
zzhou@nuc <chouzyz@gmail.com>
2016-09-18 12:49:37 +0800

    crazy by encoding

--- peacock_plan/analyzer.py
+++ peacock_plan/analyzer.py
@@ -7,19 +7,30 @@ from glob import glob
 from settings import *
 
 def unify_roster():
-	fs = glob(FPATH+'/*')
-	for f in sorted(fs, reverse=True):
-		df = pd.read_excel(f)
-		print f
-		batch = df.columns[~df.columns.str.contains('Unname')][0]
-		df.columns = df.iloc[0, :]
-		df = df.drop(df.columns.name)
-		df.index = df.iloc[:, 0]
-		df = df.drop(df.index.name, axis=1)
-		print
-		print '-----------', batch
-		print df.columns
-		
+    roster = pd.DataFrame(columns=RCOL1)
+    fs = glob(FPATH+'/*')
+    for f in sorted(fs, reverse=True):
+        df = pd.read_excel(f).fillna('NULL')
+        batch = df.columns[~df.columns.str.contains('Unname')][0]
+        df.columns = df.iloc[0, :].map(lambda s: s.replace(' ',''))
+        df = df.drop(df.columns.name)
+        df.index = df.iloc[:, 0]
+        df = df.drop(df.index.name, axis=1)
+	df[BATCH] = [batch] * df.shape[0]
+	if df.columns.shape[0] == 5:
+	    roster = pd.concat([roster, df[RCOL1]])
+	else:
+	    df_ = df[RCOL2]
+	    df_.columns = RCOL1
+	    roster = pd.concat([roster, df_])
+        print
+        print '-----------', batch
+        print f
+        print roster[RCOL1].head(1)
+	print '\t'.join(roster.columns)
+	print roster.iloc[0,:].values
+        
 
 if __name__ == '__main__':
-	unify_roster()
+    print RCOL1
+    unify_roster()
--- peacock_plan/settings.py
+++ peacock_plan/settings.py
@@ -4,3 +4,6 @@
 
 SITE  = 'http://www.szhrss.gov.cn/ztfw/gccrc/xwgg/kqjh/'
 FPATH = './roster/'
+RCOL1 = [u'工作单位', u'姓名', u'认定级别', u'主要认定依据'] + [u'批次']
+RCOL2 = [u'单位', u'姓名（拼音）', u'分类', u'符合认定的标准'] + [u'批次']
+BATCH = u'批次'
